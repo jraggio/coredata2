7385175efa9221776ede9aad83545ed3b7889119
John Raggio <jraggio@gmail.com>
2016-04-25 18:43:35 -0400

    Tutorial as specified in chapter 1.  Added entity classes, but not using them yet.

---
+++ Person+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  Person+CoreDataProperties.swift
+//  HitList
+//
+//  Created by Raggios on 4/25/16.
+//  Copyright © 2016 Raggios. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Person {
+
+    @NSManaged var name: String?
+
+}
